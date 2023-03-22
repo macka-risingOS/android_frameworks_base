d9d168fb44f93cf8d9663f73af385bd56e0e41b6
Arthur Shuai <quic_shuaiq@quicinc.com>
2023-03-22 22:12:32 +0800

    base: add support of the Phone Link
    Change-Id: I86680bc723c03f305f66bd5c5b0f110517a97504
    CRs-Fixed: 3408565

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -9551,6 +9551,7 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
 
         final int newDisplayId = getDisplayId();
         final boolean displayChanged = mLastReportedDisplayId != newDisplayId;
+        final int lastReportDisplayID = mLastReportedDisplayId;
         if (displayChanged) {
             mLastReportedDisplayId = newDisplayId;
         }
@@ -9632,7 +9633,9 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 Integer.toHexString(changes), Integer.toHexString(info.getRealConfigChanged()),
                 mLastReportedConfiguration);
 
-        if (shouldRelaunchLocked(changes, mTmpConfig) || forceNewConfig) {
+        if (shouldRelaunchLocked(changes, mTmpConfig)
+                && !mAtmService.mRemoteTaskManager.shouldIgnoreRelaunch(task, displayChanged,
+                lastReportDisplayID, newDisplayId, changes) || forceNewConfig) {
             // Aha, the activity isn't handling the change, so DIE DIE DIE.
             configChangeFlags |= changes;
             startFreezingScreenLocked(globalChanges);
