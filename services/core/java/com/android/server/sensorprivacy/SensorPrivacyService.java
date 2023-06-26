eebf73486dbafd21e4a024d11e7adafa7c458021
Shen Lin <shhenlin@gmail.com>
2023-06-26 18:21:54 +0800

    Fix system reboot in systm_server caused by SensorPrivacyService
    We should unregister all listeners from sensor privacy state change
     notifications when binder died.
    
    Test: atest SensorPrivacyServiceMockingTest , Manual
    Bug: 288750681
    Google: 2636344
    Change-Id: Ibfab9c96412e9ed09d0105eb20b1cb03105b6d44
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/sensorprivacy/SensorPrivacyService.java
+++ services/core/java/com/android/server/sensorprivacy/SensorPrivacyService.java
@@ -1501,6 +1501,7 @@ public final class SensorPrivacyService extends SystemService {
         @Override
         public void binderDied() {
             mSensorPrivacyServiceImpl.removeSensorPrivacyListener(mListener);
+            mSensorPrivacyServiceImpl.removeToggleSensorPrivacyListener(mListener);
         }
 
         public void destroy() {
