d18ee83c22fcabc1d01aab9dfa8710fa06c335d3
tanxiaoyan <tanxiaoyan@xiaomi.com>
2023-08-05 15:08:55 +0800

    Update application info for activity record when application info changed
    Activity manager will schedule application info changed for process record and app thread When OMS#setEnabled is called.
    But the app process may be restarted for top-activity, it use application of the top activity which may not be updated.
    
    Bug:294624020
    
    Change-Id: I7fac9fab11c6f62350b31e486dcea89268bd086b
    Signed-off-by: tanxiaoyan <tanxiaoyan@xiaomi.com>

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -4950,6 +4950,7 @@ public final class ProcessList {
                         if (ai != null) {
                             if (ai.packageName.equals(app.info.packageName)) {
                                 app.info = ai;
+                                app.getWindowProcessController().updateApplicationInfo(ai);
                                 PlatformCompatCache.getInstance()
                                         .onApplicationInfoChanged(ai);
                             }
