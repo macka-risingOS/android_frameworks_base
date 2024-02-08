macka-risingOS/android_frameworks_base

commit d2ea421fa6b47f6ba1bac314f77939e5ec514afc

IslandView: Fix and improve notification regex filtering
committer: rmp

test:
-produce notification starting with a special character
-systemui crashes
-apply patch
-produce same notification
-no crash observed

forwarrded by: mnri
approved by: hydrangea

Change-Id: Ic99ad5f80365a342e41fd2a92a3001dfb4973e20
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/island/IslandView.kt
@@ -281,17 +281,18 @@ class IslandView : ExtendedFloatingActionButton {
         return Pair(notifTitle.trim(), notifContent.trim())
     }
 
-    fun filterNotifContent(text: String): String {
-        val splitter = "|||"
-        val (title, content) = text.split(splitter, limit = 2)
-        var notifTitle = title.removeSuffix(":").trim()
-        notifTitle = notifTitle.replaceFirstChar {
-            if (it.isLowerCase()) it.titlecase() else it.toString() 
-        }
-        val regexPattern = Regex("\\b$notifTitle\\s*:")
-        val notifContent = content.replaceFirst(regexPattern, "").trim()
-        return "$notifTitle$splitter$notifContent"
-    }
+	fun filterNotifContent(text: String): String {
+		val splitter = "|||"
+		val (title, content) = text.split(splitter, limit = 2)
+		var notifTitle = title.removeSuffix(":").trim()
+		notifTitle = notifTitle.replaceFirstChar {
+		    if (it.isLowerCase()) it.titlecase() else it.toString() 
+		}
+
+		val regexPattern = Regex("\\b" + Regex.escape(notifTitle), setOf(RegexOption.LITERAL, RegexOption.IGNORE_CASE))
+		val notifContent = content.replaceFirst(regexPattern, "").trim()
+		return "$notifTitle$splitter$notifContent"
+	}
 
     fun getAppLabel(packageName: String, context: Context): String {
         val packageManager = context.packageManager

commit 1c7eba14fd6c7b5309d04d6d7bdfc81e54689376

Island: Improve text filtering to support symbols
* commit d2ea421fa6b47f6ba1bac314f77939e5ec514afc (by rmp) introduced a fix for preventing systemui crashes caused by invalid regex pattern,
  however, this change broke the filtering method for some instances, we've also noticed that the filtering doesnt work on words/numbers with symbols
  this CL attempts to fix the breakage caused by d2ea421fa6b47f6ba1bac314f77939e5ec514afc and finally resolve the inconsistencies of island notification text
  with applications that appends the same text used in Notification.EXTRA_TITLE and Notification.EXTRA_TEXT (as starting text/headline)

attempts to resolve the following types of notification text contents/samples:

  +123456: +123456: hello -> +123456: hello
  +123456: +123456 hello -> +123456: hello
  youtube: youtube: new video -> youtube: new video
  youtube: youtube new video -> youtube: new video
  User: user: hi -> User: hi
  Whatsapp: Whatsapp+: 1 notification --> 6. Whatsapp: 1 notification

test: manual/visual

Change-Id: Ib9a93ee633af165913026d04fc1165879f36d8d3
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/island/IslandView.kt
@@ -281,18 +281,16 @@ class IslandView : ExtendedFloatingActionButton {
         return Pair(notifTitle.trim(), notifContent.trim())
     }
 
-	fun filterNotifContent(text: String): String {
-		val splitter = "|||"
-		val (title, content) = text.split(splitter, limit = 2)
-		var notifTitle = title.removeSuffix(":").trim()
-		notifTitle = notifTitle.replaceFirstChar {
-		    if (it.isLowerCase()) it.titlecase() else it.toString() 
-		}
-
-		val regexPattern = Regex("\\b" + Regex.escape(notifTitle), setOf(RegexOption.LITERAL, RegexOption.IGNORE_CASE))
-		val notifContent = content.replaceFirst(regexPattern, "").trim()
-		return "$notifTitle$splitter$notifContent"
-	}
+    fun filterNotifContent(text: String): String {
+        val splitter = "|||"
+        val (title, content) = text.split(splitter, limit = 2)
+        var notifTitle = title.removeSuffix(":").trim()
+        notifTitle = notifTitle.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase() else it.toString() 
+        }
+        val filteredContent = content.toLowerCase().replace(notifTitle.toLowerCase(), "").trim().replaceFirst(notifTitle, notifTitle, ignoreCase = true)
+        return "$notifTitle$splitter$filteredContent"
+    }
 
     fun getAppLabel(packageName: String, context: Context): String {
         val packageManager = context.packageManager